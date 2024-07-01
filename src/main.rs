e90ed81343c9407ba80c191b75c61cce1fe64fe5
unknown <machineherald007@gmail.com>
2024-06-30 20:51:24 -0400

    refactors and updates webhook to send to multiple specified servers

--- src/main.rs
+++ src/main.rs
@@ -5,98 +5,47 @@ use dotenv::dotenv;
 use reqwest::Error;
 use serde::{Serialize, Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    id: String,
-    username: String,
-    avatar: String,
-    discriminator: String,
-    #[serde(rename = "publicFlags")]
-    public_flags: usize,
-    flags: usize,
-    banner: Option<String>,
-    #[serde(rename = "accentColor")]
-    accent_color: Option<usize>,
-    #[serde(rename = "globalName")]
-    global_name: Option<String>,
-    #[serde(rename = "avatarDecoration")]
-    avatar_decoration: Option<String>,
-    #[serde(rename = "")]
-    display_name: Option<String>,
-    #[serde(rename = "bannerColor")]
-    banner_color: Option<String>
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct PartialGuildObject {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct PartialChannelObject {}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct WebhookObject {
-    id: String,
-    r#type: u32,
-    #[serde(rename = "guildId")]
-    guild_id: String,
-    #[serde(rename = "channelId")]
-    channel_id: String,
-    user: User,
-    name: String,
-    avatar: String,
-    token: String,
-    #[serde(rename = "applicationId")]
-    application_id: String,
-    #[serde(rename = "sourceGuild")]
-    source_guild: PartialGuildObject,
-    #[serde(rename = "sourceChannel")]
-    source_channel: PartialChannelObject,
-    url: String
-}
-
 #[derive(Debug, Deserialize, Serialize)]
-struct ExecuteWebhook {
+struct DiscordPayload {
     content: String
 }
 
-async fn get_webhook_obj() -> Result<(), Error> {
-    let api_endpoint = std::env::var("API_ENDPOINT").expect("API_ENDPOINT must be set.");
-    let webhook_id = std::env::var("BANNER_UPDATES_WEBHOOK_ID").expect("WEBHOOK_ID must be set.");
-    let url = api_endpoint.to_owned() + &webhook_id;
+async fn send_banners(webhook_urls: Vec<String>, banner_obj: DiscordPayload) -> Result<(), Error> {
+    for url in webhook_urls {
+        let client = reqwest::Client::new();
+        let response =
+            client.post(url)
+            .header("Content-Type", "application/json")
+            .json(&banner_obj)
+            .send()
+            .await?
+        ;
+    }
 
-    let response: WebhookObject =
-        reqwest::get(url)
-        .await?
-        .json()
-        .await?
-    ;
-
-    println!("{:?}", response);
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     dotenv().ok();
-    let args: Vec<String> = env::args().collect();
     let api_endpoint = std::env::var("API_ENDPOINT").expect("API_ENDPOINT must be set.");
-    let webhook_id = std::env::var("BANNER_UPDATES_WEBHOOK_ID").expect("WEBHOOK_ID must be set.");
-    let webhook_token = std::env::var("BANNER_UPDATES_WEBHOOK_TOKEN").expect("WEBHOOK_TOKEN must be set.");
-    let url = api_endpoint.to_owned() + &webhook_id + "/" + &webhook_token;
     
-    //This is the JSON payload we're sending to Discord Webhook
-    let banner_obj = ExecuteWebhook {
+    let black_order_id = std::env::var("BLACK_ORDER_WEBHOOK_ID").expect("WEBHOOK_ID must be set.");
+    let black_order_token = std::env::var("BLACK_ORDER_WEBHOOK_TOKEN").expect("WEBHOOK_TOKEN must be set.");
+
+    let kitsune_id = std::env::var("KITSUNE_WEBHOOK_ID").expect("WEBHOOK_ID must be set.");
+    let kitsune_token = std::env::var("KITSUNE_WEBHOOK_TOKEN").expect("WEBHOOK_TOKEN must be set.");
+
+    let mut webhook_urls = Vec::new();
+    webhook_urls.push(api_endpoint.to_owned() + &black_order_id + "/" + &black_order_token);
+    webhook_urls.push(api_endpoint.to_owned() + &kitsune_id + "/" + &kitsune_token);
+
+    let args: Vec<String> = env::args().collect();
+    let banner_obj = DiscordPayload {
         content: String::from(&args[1])
     };
 
-    let client = reqwest::Client::new();
-    let response =
-        client.post(url)
-        .header("Content-Type", "application/json")
-        .json(&banner_obj)
-        .send()
-        .await?
-    ;
+    send_banners(webhook_urls, banner_obj).await?;
 
     Ok(())
 }
